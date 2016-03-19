523c5816bb205e80adc43945688e3307c3094463
root <efraimgentil@gmail.com>
2016-03-19 10:40:43 -0300

    Removendo testes desnecessarios

--- src/test/java/br/edu/fa7/gestaopessoas/apresentacao/InserirPessoaEVincularADepartamentoCargo.java
+++ src/test/java/br/edu/fa7/gestaopessoas/apresentacao/InserirPessoaEVincularADepartamentoCargo.java
@@ -1,52 +1,52 @@
 package br.edu.fa7.gestaopessoas.apresentacao;
 
-import br.edu.fa7.gestaopessoas.models.Pessoa;
-import br.edu.fa7.gestaopessoas.models.PessoaFisica;
+import br.edu.fa7.gestaopessoas.factory.HibernateFactory;
+import br.edu.fa7.gestaopessoas.models.*;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import org.junit.AfterClass;
 import org.springframework.test.annotation.Rollback;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 /**
  * Created by efraimgentil<efraimgentil@gmail.com> on 17/03/16.
  */
-public class InserirPessoaEVincular {
+public class InserirPessoaEVincularADepartamentoCargo {
 
-  SessionFactory sessionFactory;
   Session session;
 
-  @BeforeClass
-  public void setUp(){
-    sessionFactory = HibernateUtil.getSessionFactory();
-  }
-  @AfterClass
-  public void destroy(){
-    sessionFactory.close();
-  }
-
   @BeforeMethod
-  public void setUpTest(){
-    session = sessionFactory.openSession();
+  public void setUpTest() throws Exception {
+    session = HibernateFactory.getHibernateSession();
+    session.beginTransaction();
   }
 
   @AfterMethod
+  public void finishTest(){
+    session.getTransaction().commit();
+  }
+
+  @AfterClass
   public void destroyTest(){
-    session.close();
+    HibernateFactory.closeFactory();
   }
 
   @Test( priority = 1 , description = "Tentara salvar uma pessoa de nome Efraim Gentil")
   public void devePersistirAPessoaEfraim(){
-    session.getTransaction().begin();
-    Pessoa p = new PessoaFisica();
+    PessoaFisica p = new PessoaFisica();
     p.setName("Efraim Gentil");
+    p.setCpf("11234421234");
+    p.setDataNascimento(new Date());
     session.persist(p);
-    session.getTransaction().commit();
   }
 
   @Test( priority = 2 , description = "Garante que a pessoa de nome Efraim Gentil foi persistida")
@@ -60,77 +60,73 @@ public class InserirPessoaEVincular {
       assertTrue( p.getName().toUpperCase().contains("EFRAIM") );
     }
   }
-/*
-  @Rollback(false)
+
   @Test( priority = 3 , description = "Cadastra um cargo programador")
   public void devePersistirUmCargo(){
     Cargo cargo = new Cargo();
     cargo.setNome("Programador");
-    cargo.setSalario( new BigDecimal(999999.999999));
-    entityManager.persist( cargo );
+    cargo.setSalario(new BigDecimal(999999.999999));
+    session.persist(cargo);
   }
 
-  @Rollback(false)
   @Test( priority = 4 , description = "Cadastra um departamento")
   public void devePersistirUmDepartamento(){
     Departamento departamento = new Departamento();
     departamento.setNome("ASTIN");
-    entityManager.persist( departamento );
+    session.persist(departamento);
   }
 
-  @Rollback(false)
   @Test( priority = 5 , description = "Deve criar um vinculo para a pessoa " +
           "Efraim de cargo Programador no departamento ASTIN")
   public void devePersistirUmVinculoParaAPessoa(){
-    TypedQuery<Pessoa> query = entityManager.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome", Pessoa.class);
+    Query query = session.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome");
     query.setParameter("nome", "EFRAIM%");
-    Pessoa pessoa  = query.getSingleResult();
+    Pessoa pessoa  = (Pessoa) query.list().get(0);
 
-    TypedQuery<Cargo> queryCargo = entityManager.createQuery("FROM Cargo a WHERE upper(a.nome) like :nome", Cargo.class);
+    Query queryCargo = session.createQuery("FROM Cargo a WHERE upper(a.nome) like :nome");
     queryCargo.setParameter("nome", "PROGRAMADOR%");
-    Cargo cargo  = queryCargo.getSingleResult();
+    Cargo cargo  = (Cargo) queryCargo.list().get(0);
 
-    TypedQuery<Departamento> queryDepartamento = entityManager.createQuery("FROM Departamento a WHERE upper(a.nome) like :nome", Departamento.class);
+    Query queryDepartamento = session.createQuery("FROM Departamento a WHERE upper(a.nome) like :nome");
     queryDepartamento.setParameter("nome", "ASTIN%");
-    Departamento departamento = queryDepartamento.getSingleResult();
+    Departamento departamento = (Departamento) queryDepartamento.list().get(0);
 
     pessoa.novoVinculo( departamento , cargo );
 
-    entityManager.merge(pessoa);
+    session.merge(pessoa);
   }
 
   @Test( priority = 6 , description = "Deve consulta pessoa Efraim e garantir que vinculoAtual esteja preenchido")
   public void deveGarantirQueAPessoaPossuiVinculoAtual(){
-    TypedQuery<Pessoa> query = entityManager.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome", Pessoa.class);
+    Query query = session.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome");
     query.setParameter("nome", "EFRAIM%");
-    Pessoa pessoa  = query.getSingleResult();
+    Pessoa pessoa  = (Pessoa) query.list().get(0);
 
     assertNotNull(pessoa.getVinculoAtual());
     assertEquals(pessoa.getVinculoAtual().getId(), pessoa.getVinculos().get(0).getId());
   }
 
-  @Rollback(false)
   @Test( priority = 7 , description = "Deve fechar o vinculo atual da pessoa Efraim")
   public void deveGarantirQueAPessoaFoiAtualizadaJuntoComVinculo(){
-    TypedQuery<Pessoa> query = entityManager.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome", Pessoa.class);
+    Query query = session.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome");
     query.setParameter("nome", "EFRAIM%");
-    Pessoa pessoa  = query.getSingleResult();
+    Pessoa pessoa  = (Pessoa) query.list().get(0);
 
     pessoa.fechaVinculoAtual();
 
-    entityManager.merge( pessoa );
+    session.merge( pessoa );
   }
 
   @Test( priority = 8 , description = "Deve deve garantir que o vinculo da pessoa efraim foi fechado")
   public void deveGarantirQueAPessoaEfraimTeveSeuVinculoFechado(){
-    TypedQuery<Pessoa> query = entityManager.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome", Pessoa.class);
+    Query query = session.createQuery("FROM Pessoa a WHERE upper(a.name) like :nome");
     query.setParameter("nome", "EFRAIM%");
-    Pessoa pessoa  = query.getSingleResult();
+    Pessoa pessoa  = (Pessoa) query.list().get(0);
 
     assertNull( pessoa.getVinculoAtual() );
     for(Vinculo vinculo : pessoa.getVinculos()){
       assertNotNull( vinculo.getFim() );
     }
-  }*/
+  }
 
 }
